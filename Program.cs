f64e84e64c33af4b56c03a130525b2fac417d983
wilsonmanzano-desing <wilsonmanzanosis@gmail.com>
2025-08-28 14:51:17 -0500

    actualizacion

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
 ﻿using System;
 
+class Program
+{
+    static void Main()
+    {
+        Random aleatorio = new Random();
+        int numeroSecreto = aleatorio.Next(1, 21); // Número entre 1 y 20
+        int intento;
+        int i = 0;
+        
+        Console.WriteLine("¡Adivina el número secreto entre 1 y 20!");
 
-Console.WriteLine("Hola mundo");
+        do
+        {
+            Console.Write("Ingresa tu intento: ");
+            intento = Convert.ToInt32(Console.ReadLine());
+
+            if (intento > numeroSecreto)
+            {
+                Console.WriteLine("El número secreto es menor.");
+            }
+            else if (intento < numeroSecreto)
+            {
+                Console.WriteLine("El número secreto es mayor.");
+            }
+            else
+            {
+                Console.WriteLine("¡Correcto! El número secreto era " + numeroSecreto);
+            }
+            i++;
+            Console.WriteLine("numero de intentos " + i);
+
+        } while (intento != numeroSecreto && i <= 5);
+    }
+}
