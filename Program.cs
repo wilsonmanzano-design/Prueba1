cebb50e537e82acff534a75c8f083e69d85f02d2
wilsonmanzano-desing <wilsonmanzanosis@gmail.com>
2025-08-28 14:47:02 -0500

    nueva prueba de git

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Este es un nuevo programa github");
+﻿using System;
+
+
+Console.WriteLine("Hola mundo");
